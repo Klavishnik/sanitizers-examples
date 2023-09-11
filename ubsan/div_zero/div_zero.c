02380cc240c9803b2079f34c2bc0d9ad04f81046
Klavishnik <evg.shtanov@gmail.com>
2023-09-11 15:33:30 +0300

    add ubsan

---
+++ ubsan/div_zero/div_zero.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 0;
+    int result = a / b;  // деление на ноль
+    printf("result: %d\n", result);
+    return 0;
+}
---
+++ ubsan/overflow/int_overflow.c
@@ -0,0 +1,9 @@
+#include <limits.h>
+#include <stdio.h>
+
+int main() {
+    int max_value = INT_MAX;
+    int reuslt = max_value * max_value;
+    printf("%i", max_value);
+    return 0;
+}
